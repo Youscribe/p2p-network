1597b43442afe5f4c8341e3f2924e89ef307ca2b
Guilhem Lettron <guilhem@lettron.fr>
2013-07-16 12:19:09 +0200

    release v0.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "guilhem.lettron@youscribe.com"
 license          "Apache 2.0"
 description      "Configure an p2p network between nodes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 depends          "tunnel"
